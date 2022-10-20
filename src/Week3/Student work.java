e153cf71975e013bb055e6f51b83b356320529f0
Imaan19 <114931648+Imaan19@users.noreply.github.com>
2022-10-20 16:43:19 +0100

    Add files via upload

---
+++ src/Week3/Student work.java
@@ -0,0 +1,25 @@
+package Week3;
+
+public class Student 
+{
+    private int id;
+    private String name;
+
+    //Constructor
+    public Student(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+    
+    //print method
+    public void print()
+    {
+        System.out.println("ID; " + this.id + "name " + this.name);
+    }
+
+
+
+
+
+}   
