1be8fa5e5064510b8fdaffbd1350c383cb90d44d
Imaan19 <Imaanmajid@icloud.com>
2022-11-01 11:06:58 +0000

    Coursework

---
+++ src/Week3/Coursework/Number 1.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList; // import the ArrayList class
+
+ArrayList<String> song title = new ArrayList<String>(); // Create an ArrayList object
+
+public class Song
+{
+    private String Song title;
+    private String artist name;
+    private int play count;
+
+    //Constructor
+    public Song:String title, String artist name, int play count
+    [
+        this.title = 
+    ]
+}
+    
+    public Song(String songtitle, String artistname, int play count)
+    
+       
--- src/Week3/Week03.java
+++ src/Week3/Week03.java
@@ -1,5 +1,7 @@
 package Week3;
 
+import java.util.ArrayList;
+
 public class Week03 
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
@@ -15,6 +17,25 @@ public class Week03
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
+
+        
+        Student imaan = new Student ( 22201497, "Imaan");
+        //imaan.print();
+
+        Student nick = new Student (123456, "Nick");
+        //nick.print();
+
+        ArrayList<Student> students = new ArrayList<Student>();
+
+        students.add(imaan);
+        students.add(nick);
+
+        for (Student student: students)
+        {
+            student.print();
+        }
     }     
     
 }
+
+    
